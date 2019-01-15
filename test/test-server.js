5988128448322ee3aed7c91cdf08f674cee2a6a9
Julie Hull <juliehullwebdev@gmail.com>
2019-01-15 11:20:05 -0800

    remove extra root file

--- test/test-server.js
+++ test/test-server.js
@@ -1,34 +1,21 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, closeServer} = require('../server');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Users', function() {
+describe('Root', function() {
 
-  before(function() {
-    return runServer();
- 
-  after(function() {
-    return closeServer();
-  });
+
   
-  it('should list users on GET', function() {
-    return chai.request(app)
-      .get('/users')
+  it('should show HTML for the root', function() {
+    return chai.request('http://localhost:8080/')
+      .get('/')
       .then(function(res) {
         expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body.length).to.be.above(0);
-        res.body.forEach(function(item) {
-          expect(item).to.be.a('object');
-          expect(item).to.have.all.keys(
-            'id', 'firstName', 'lastName');
-        });
+        expect(res).to.be.html
       });
   });
 });
