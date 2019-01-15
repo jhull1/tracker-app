29f5b51507c842d9e66f110e90d4cad765b7ad87
Julie Hull <juliehullwebdev@gmail.com>
2019-01-15 10:41:11 -0800

    link with travis

---
+++ test/test-server.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Users', function() {
+
+  before(function() {
+    return runServer();
+ 
+  after(function() {
+    return closeServer();
+  });
+  
+  it('should list users on GET', function() {
+    return chai.request(app)
+      .get('/users')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.above(0);
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.have.all.keys(
+            'id', 'firstName', 'lastName');
+        });
+      });
+  });
+});
