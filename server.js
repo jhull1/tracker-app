83944d50d1e2735dd29dfedeaf215c3acfebac9c
Julie Hull <juliehullwebdev@gmail.com>
2019-01-17 09:44:03 -0800

    add routes for additional landing pages

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+app.get('/user-stats', (req, res) => {
+  res.sendFile('user-stats.html');
+});
+
+app.get('/new-post', (req, res) => {
+  res.sendFile('new-post.html');
+});
+
 app.listen(process.env.PORT || 8080);
